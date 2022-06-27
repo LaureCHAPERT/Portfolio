42767e9a3dbb01c5bc74638deb436d572954fcec
LaureCHAPERT <laure.chapert@gmail.com>
2022-06-27 12:10:24 +0200

    adding layouts

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -38,14 +38,3 @@
             </ul>
         </div>
     </nav>
-        <div class="zoomIn justify-content-evenly">
-            <h1 >{ Bonjour ! } </h1>
-            <p>Je suis Laure Chapert, récemment diplômée du diplôme de développeur web et web mobile et en pleine recherche d'emploi en full remote idéalement ! </p>
-        </div>
-    </body>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</html>
