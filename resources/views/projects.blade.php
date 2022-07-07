58f0450a6adf23cdc45a52a6b1efdf3f660b00a5
LaureCHAPERT <laure.chapert@gmail.com>
2022-07-07 16:11:21 +0200

    small changes

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,6 +1,7 @@
 @include('layouts/navbar')
-    <h1 class="text-center text-decoration-underline"> <span class="the-span"> --- Projet de fin de formation : Concert'O ---</span></h1>
+    <h1 class="text-center text-decoration-underline"> <span class="the-span "> Projet de fin de formation : Concert'O </span></h1>
         <div class="container zoomIn text-center justify-content-space-between">
+        {{--First row with 12 columns (grid bootstrap)--}}
             <div class="row">
                 <div class="col-lg-6">
                     <div class="embed-responsive embed-responsive-16by9">
@@ -16,6 +17,7 @@
             </div>
         </div>
     <div class="container zoomIn bolder" >
+         {{--Second row with 12 columns (grid bootstrap)--}}
         <div class="row">
             <div class="col-lg-6">
                 <h2 class= "text-decoration-underline">Technologies utilisées côté front : </br> </h2>
@@ -65,12 +67,14 @@
                 </div>
         </div>
     </div>
+    {{--To avoid the repetition of <br> we create an empty div with a defined height.--}}
   <div style="height:100px;display:block;"> </div>
 
   <h1 class="text-center text-decoration-underline"> <span class="the-span"> Ce même projet de portfolio 🐾 </span> </h1>
     <div class="container zoomIn bolder text-center" >
         <div class="row">
             <div>
+
                 <h2 class= "text-decoration-underline">Technologies utilisées : </h2> </br>
                     <p>  L'idée était de me familiariser avec <strong>Laravel</strong> que je connaissais peu. J'ai déjà pu travailler avec Symfony lors de notre projet de fin de formation avec mes collègues qui travaillaient sur la partie back-end. Lors de ma formation, nous avons également expérimenté Lumen sur quelques jours mais je souhaitais approfondir cela en partant, cette fois sur Laravel. Ce portfolio est un exemple très simple, mais je continue mon apprentissage à travers un autre projet guidé sur Laracasts, notamment en manipulant une base de données.
                     Pour ce qui est du style, je continue à travailler avec <strong>Sass</strong> et <strong> Bootstrap</strong>.
