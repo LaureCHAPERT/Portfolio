f2e759b55a7de8a10e29a2de53c8269dc0d24975
LaureCHAPERT <laure.chapert@gmail.com>
2022-07-17 17:19:26 +0200

    responsive mode is okay on about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,7 @@
         <div class="about container-fluid zoomIn">
         <h1> Mon parcours </h1>
                     <div class="about-first">
+                    <div class="about-first-responsive">
 
                         <p>
                         Anciennement technicienne du son et plus récemment conseillère de vente en multimédia, j'ai entamé une reconversion professionnelle dans un domaine qui me passionne depuis toujours : l'informatique. <br>
@@ -10,6 +11,7 @@
 
                             <img class="about-first-img-shiba" src="{{ asset('images/shiba.svg') }}" alt="decorative image with shiba">
 
+                    </div>
                     </div>
                     <h1> Ma formation </h1>
                     <div class="about-first">
@@ -24,13 +26,15 @@
                     </div>
                     <h1> Et maintenant ...? </h1>
                     <div class="about-first">
+                    <div class="about-first-responsive">
 
                         <p>
                         Pour finaliser notre formation, nous avons dû créer un projet en équipe d'A à Z sur un mois complet. J'ai travaillé sur la partie front avec React et j'ai eu la chance de porter deux casquettes pendant cette période de travail ce qui fût très enrichissant : celle de Lead Dev Front et celle de Product Owner étant l'instigatrice du projet. Lors des phases de travaux en équipe où je rejoignais l'équipe back, j'ai également pu me familiariser avec le framework Symfony. Je poursuis encore mon apprentissage en ce moment en réalisant ce portfolio et un autre sur Laravel. Je m'apprête également à passer ma certification Opquast. Pour plus de détails sur mes compétences merci de cliquer ici :
                         </p>
                         <div class="about-first-img">
-                            <img class="about-first-img-octopus" src="{{ asset('images/octopus.svg') }}" alt="decorative image with octopus">
+                            <img class="about-first-img-dog" src="{{ asset('images/dog.svg') }}" alt="decorative image with dog">
                         </div>
+                    </div>
                     </div>
 
         </div>
