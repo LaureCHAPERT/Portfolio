063ec6d6d1ff0ccf1bc52a3ac3fb9a5c981d2fd2
LaureCHAPERT <laure.chapert@gmail.com>
2022-07-17 17:36:46 +0200

    modifications

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
 
                         <p>
                         Anciennement technicienne du son et plus récemment conseillère de vente en multimédia, j'ai entamé une reconversion professionnelle dans un domaine qui me passionne depuis toujours : l'informatique. <br>
-                        J'ai suivi une formation intensive chez O'clock afin d'y parvenir : celle de développeur web et web mobile basé sur un socle orienté PHP qui constitue la première partie de l'apprentissage. Ensuite, j'ai dû choisir une spécialisation. Ayant une certaine curiosité pour JavaScript, j'ai choisi la bibliothèque React. </p>
+                        J'ai suivi une formation intensive chez O'clock afin d'y parvenir : celle de développeur web et web mobile basé sur un socle orienté PHP qui constitue la première partie de l'apprentissage. </p>
 
                             <img class="about-first-img-shiba" src="{{ asset('images/shiba.svg') }}" alt="decorative image with shiba">
 
@@ -20,7 +20,9 @@
                             <img class="about-first-img-dango" src="{{ asset('images/dango.svg') }}" alt="decorative image with cat eating dango">
                         </div>
                         <p>
-                        Pour finaliser notre formation, nous avons dû créer un projet en équipe d'A à Z sur un mois complet. J'ai travaillé sur la partie front avec React et j'ai eu la chance de porter deux casquettes pendant cette période de travail ce qui fût très enrichissant : celle de Lead Dev Front et celle de Product Owner étant l'instigatrice du projet. Lors des phases de travaux en équipe où je rejoignais l'équipe back, j'ai également pu me familiariser avec le framework Symfony. Je poursuis encore mon apprentissage en ce moment en réalisant ce portfolio et un autre sur Laravel. Je m'apprête également à passer ma certification Opquast. Pour plus de détails sur mes compétences merci de cliquer ici :
+                        Durant le socle de la formation, on apprend les bases avec HTML5, CSS3, JavaScript et PHP. On se familiarise même avec certains outils comme Bootstrap, Lumen ou encore l'outil de gestion Trello.
+                        Ensuite, j'ai dû choisir une spécialisation. Ayant une certaine curiosité pour JavaScript, j'ai orienté mon choix sur la bibliothèque React.
+                        Pour finaliser notre formation, nous avons dû créer un projet en équipe de A à Z sur un mois complet. J'ai travaillé sur la partie front avec React et j'ai eu la chance de porter deux casquettes pendant cette période de travail ce qui fût très enrichissant : celle de Lead Dev Front et celle de Product Owner étant l'instigatrice du projet. Lors des phases de travaux en équipe où je rejoignais l'équipe back, j'ai également pu me familiariser avec le framework Symfony. Je poursuis encore mon apprentissage en ce moment en réalisant ce portfolio et un autre sur Laravel. Je m'apprête également à passer ma certification Opquast. Pour plus de détails sur mes compétences, rejoignez la page qui y est consacrée :<a href="/competences">juste ICI !</a>
                         </p>
 
                     </div>
@@ -29,7 +31,7 @@
                     <div class="about-first-responsive">
 
                         <p>
-                        Pour finaliser notre formation, nous avons dû créer un projet en équipe d'A à Z sur un mois complet. J'ai travaillé sur la partie front avec React et j'ai eu la chance de porter deux casquettes pendant cette période de travail ce qui fût très enrichissant : celle de Lead Dev Front et celle de Product Owner étant l'instigatrice du projet. Lors des phases de travaux en équipe où je rejoignais l'équipe back, j'ai également pu me familiariser avec le framework Symfony. Je poursuis encore mon apprentissage en ce moment en réalisant ce portfolio et un autre sur Laravel. Je m'apprête également à passer ma certification Opquast. Pour plus de détails sur mes compétences merci de cliquer ici :
+                        Désormais, je recherche un emploi dès maintenant en full remote afin de mettre en pratique mes connaissances et de monter en compétences! Le métier de développeur (tant côté back que front) est très riche et inépuisable, ce qui consitue un réel moteur à ma motivation!
                         </p>
                         <div class="about-first-img">
                             <img class="about-first-img-dog" src="{{ asset('images/dog.svg') }}" alt="decorative image with dog">
