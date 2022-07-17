5cf4d2749f6dcae0df89e47371a2097bbbfc8d85
LaureCHAPERT <laure.chapert@gmail.com>
2022-07-17 16:45:05 +0200

    about page's integration is okay but not responsive yet

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,30 +1,38 @@
 @include('layouts/navbar')
 
         <div class="about container-fluid zoomIn">
+        <h1> Mon parcours </h1>
                     <div class="about-first">
-                        <div class="about-first-img">
-                            <img class="about-first-img-shiba" src="{{ asset('images/shiba.svg') }}" alt="decorative image with shiba">
-                        </div>
+
                         <p>
                         Anciennement technicienne du son et plus récemment conseillère de vente en multimédia, j'ai entamé une reconversion professionnelle dans un domaine qui me passionne depuis toujours : l'informatique. <br>
                         J'ai suivi une formation intensive chez O'clock afin d'y parvenir : celle de développeur web et web mobile basé sur un socle orienté PHP qui constitue la première partie de l'apprentissage. Ensuite, j'ai dû choisir une spécialisation. Ayant une certaine curiosité pour JavaScript, j'ai choisi la bibliothèque React. </p>
+
+                            <img class="about-first-img-shiba" src="{{ asset('images/shiba.svg') }}" alt="decorative image with shiba">
+
                     </div>
+                    <h1> Ma formation </h1>
                     <div class="about-first">
-                        <p>
-                        Pour finaliser notre formation, nous avons dû créer un projet en équipe d'A à Z sur un mois complet. J'ai travaillé sur la partie front avec React et j'ai eu la chance de porter deux casquettes pendant cette période de travail ce qui fût très enrichissant : celle de Lead Dev Front et celle de Product Owner étant l'instigatrice du projet. Lors des phases de travaux en équipe où je rejoignais l'équipe back, j'ai également pu me familiariser avec le framework Symfony. Je poursuis encore mon apprentissage en ce moment en réalisant ce portfolio et un autre sur Laravel. Je m'apprête également à passer ma certification Opquast. Pour plus de détails sur mes compétences merci de cliquer ici :
-                        </p>
-                        <div class="about-first-img">
-                            <img class="about-first-img-shiba" src="{{ asset('images/dango.svg') }}" alt="decorative image with cat eating dango">
-                        </div>
-                    </div>
-                    <div class="about-first">
-                        <div class="about-first-img">
-                            <img class="about-first-img-shiba" src="{{ asset('images/octopus.svg') }}" alt="decorative image with octopus">
+
+                    <div class="about-first-img">
+                            <img class="about-first-img-dango" src="{{ asset('images/dango.svg') }}" alt="decorative image with cat eating dango">
                         </div>
                         <p>
                         Pour finaliser notre formation, nous avons dû créer un projet en équipe d'A à Z sur un mois complet. J'ai travaillé sur la partie front avec React et j'ai eu la chance de porter deux casquettes pendant cette période de travail ce qui fût très enrichissant : celle de Lead Dev Front et celle de Product Owner étant l'instigatrice du projet. Lors des phases de travaux en équipe où je rejoignais l'équipe back, j'ai également pu me familiariser avec le framework Symfony. Je poursuis encore mon apprentissage en ce moment en réalisant ce portfolio et un autre sur Laravel. Je m'apprête également à passer ma certification Opquast. Pour plus de détails sur mes compétences merci de cliquer ici :
                         </p>
+
+                    </div>
+                    <h1> Et maintenant ...? </h1>
+                    <div class="about-first">
+
+                        <p>
+                        Pour finaliser notre formation, nous avons dû créer un projet en équipe d'A à Z sur un mois complet. J'ai travaillé sur la partie front avec React et j'ai eu la chance de porter deux casquettes pendant cette période de travail ce qui fût très enrichissant : celle de Lead Dev Front et celle de Product Owner étant l'instigatrice du projet. Lors des phases de travaux en équipe où je rejoignais l'équipe back, j'ai également pu me familiariser avec le framework Symfony. Je poursuis encore mon apprentissage en ce moment en réalisant ce portfolio et un autre sur Laravel. Je m'apprête également à passer ma certification Opquast. Pour plus de détails sur mes compétences merci de cliquer ici :
+                        </p>
+                        <div class="about-first-img">
+                            <img class="about-first-img-octopus" src="{{ asset('images/octopus.svg') }}" alt="decorative image with octopus">
+                        </div>
                     </div>
 
         </div>
+  </div>
 @include ('layouts/footer')
