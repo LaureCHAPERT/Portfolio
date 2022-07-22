fc1d1b712a51aeffe667ca6ae6eaef593c8343a7
LaureCHAPERT <laure.chapert@gmail.com>
2022-07-22 09:09:21 +0200

    Adding favicon

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Mon Portfolio</title>
-
+        <link rel="shortcut icon" sizes="114x114" href="{{ asset('images/favicon.ico') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
